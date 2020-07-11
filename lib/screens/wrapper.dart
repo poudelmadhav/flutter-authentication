df28780a0934019bb920d82954fccc5ec2bbad16
Madhav Paudel <poudelmadhav143@gmail.com>
2020-07-11 23:22:42 +0545

    sign in anonymosly

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -6,6 +6,6 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return either Home or Authenticate widget
-    return Home();
+    return Authenticate();
   }
 }
