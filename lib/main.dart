0103d395f73b0b36ba88829cdea1cc505bb0ea06
Madhav Paudel <poudelmadhav143@gmail.com>
2020-07-12 00:12:33 +0545

    user stream if signed in or not using provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:madhav_auth/models/user.dart';
 import 'package:madhav_auth/screens/wrapper.dart';
+import 'package:madhav_auth/services/auth.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,8 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:madhav_auth/models/user.dart';
 import 'package:madhav_auth/screens/authenticate/authenticate.dart';
 import 'package:madhav_auth/screens/home/home.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print(user);
+
     // return either Home or Authenticate widget
     return Authenticate();
   }
