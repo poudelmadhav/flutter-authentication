585a6dfb1ed29610e2fbe7149402c2cd24be810c
Madhav Paudel <poudelmadhav143@gmail.com>
2020-07-12 00:26:44 +0545

    dynamically show page based on sign in and sign out

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:madhav_auth/services/auth.dart';
 
 class Home extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Home'),
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        title: Text('Madhav Auth'),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
+            label: Text(
+              'Logout',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () async {
+              await _auth.signOut();
+            },
+          )
+        ],
+      ),
     );
   }
 }
