0a3f6a34ba5f4ad68cc90a31074d9af33cab61a0
Madhav Paudel <poudelmadhav143@gmail.com>
2020-07-12 01:48:47 +0545

    register user from form to firebase

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -40,7 +40,7 @@ class _SignInState extends State<SignIn> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
-          child: Column(
+          child: ListView(
             children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
