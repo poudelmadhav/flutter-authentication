1e7c4a4690873f267ce02fa1a24f3eeec0f62c8b
Madhav Paudel <poudelmadhav143@gmail.com>
2020-07-14 00:18:17 +0545

    get firestore data using stream

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,34 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:madhav_auth/screens/home/brew_list.dart';
 import 'package:madhav_auth/services/auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:madhav_auth/services/database.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text('Madhav Auth'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(
-              Icons.person,
-              color: Colors.white,
-            ),
-            label: Text(
-              'Logout',
-              style: TextStyle(
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Madhav Auth'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(
+                Icons.person,
                 color: Colors.white,
               ),
-            ),
-            onPressed: () async {
-              await _auth.signOut();
-            },
-          )
-        ],
+              label: Text(
+                'Logout',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            )
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
