bec23c054ce08959ca1900ccdeba96093d28b9e7
Madhav Paudel <poudelmadhav143@gmail.com>
2020-07-14 22:15:25 +0545

    modal bottom sheets

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -10,6 +10,18 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+            child: Text('bottom sheet'),
+          );
+        },
+      );
+    }
+
     return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
@@ -33,6 +45,19 @@ class Home extends StatelessWidget {
               onPressed: () async {
                 await _auth.signOut();
               },
+            ),
+            FlatButton.icon(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              label: Text(
+                'settings',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () => _showSettingsPanel(),
             )
           ],
         ),
